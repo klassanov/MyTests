eb777530ec7fdd028e97f75934fcb7d9dda574b0
Alexander Klassanov <klassanov@gmail.com>
2019-04-22 23:22:44 +0300

    Add project files.

---
+++ MyTests/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test();
+        }
+
+
+        static void Test()
+        {
+            try
+            {
+                Console.WriteLine("Try block");
+                throw new Exception("Test");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Catch block");
+                //throw new Exception("My Exception");
+            }
+            finally
+            {
+                Console.WriteLine("Finally block");
+            }
+            Console.ReadKey();
+        }
+
+
+
+
+    }
+}
